3d656aeefe5079d8cf7345d9fb20ff9ef105635e
J. Nathanael Philipp <nathanael@philipp.land>
2022-11-03 15:46:38 +0100

    Bump to 0.3.0.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read_file(name):
 
 setup(
     name="django_bootstrap5",
-    version="0.2.2",
+    version="0.3.0",
     description="Add Bootstrap 5 to Django.",
     author="J. Nathanael Philipp",
     author_email="nathanael@philipp.land",
